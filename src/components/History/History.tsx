702ecfa3435881d74c56e187325817a28bd3f92e
EkaterinaToporkova <toporkovaekaterina5@gmail.com>
2024-06-20 20:39:40 +0300

    Added tape.svg icon to history component and positioned it - Integrated tape.svg icon into the history component for visual enhancement. - Positioned the icon using absolute positioning to overlay it neatly on the component's background. - Adjusted positioning attributes (top and left) to achieve the desired visual alignment within the component.

--- src/components/History/History.tsx
+++ src/components/History/History.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './History.module.css'
 import close from "../../assets/icon/close.svg"
+import tape from "../../assets/icon/tape.svg"
+
 
 export const History = () => {
     return (
         <div className={styles.history_content}>
                 <div className={styles.history}>
+                <img src={tape} alt="Tape" className={styles.overlay__image}/>
                   <h4>История</h4>
                   <ul className={styles.history__items}>
                     <li className={styles.history__item}>
@@ -14,7 +17,7 @@ export const History = () => {
                     <li className={styles.history__item}>200 грамм воды = 1 стакан</li>
                   </ul>
                   <div className="reset__btn">
-                    <img src= {close} alt="" />
+                    <img src={close} alt="" />
                     <input
                       type="reset"
                       value="Очистить историю"
